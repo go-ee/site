1ce16028ca7c903fd4f6d4a0c9af3fab8e58c8b7
Eugen Eisler <eugen.eisler@siemens.com>
2020-11-07 13:50:39 +0100

    Improve APIs

--- go.mod
+++ go.mod
@@ -5,8 +5,10 @@ go 1.15
 require github.com/urfave/cli/v2 v2.3.0
 
 require (
+	github.com/go-ee/emailbridge v0.0.0-20201104235635-9c6594b08df8
 	github.com/go-ee/utils v0.0.0-20201104184309-5b62a7627986
 	github.com/sirupsen/logrus v1.7.0
 )
 
 replace github.com/go-ee/utils => ../utils/
+replace github.com/go-ee/emailbridge => ../emailbridge/
--- site.go
+++ site.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-	"github.com/go-ee/utils/email"
+	"github.com/go-ee/emailbridge"
 	"github.com/go-ee/utils/lg"
 	"github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 import (
 	"fmt"
 	"github.com/urfave/cli/v2"
-	"log"
-	"os"
 )
 
 func main() {
-	var emailAddress, smtpLogin, smtpPassword, smtpHost, address, root, targetFile string
-	var port, smtpPort int
+	var server, root, configFile, targetFile, cors string
 	var debug bool
+	var port int
 
 	app := cli.NewApp()
 	app.Name = "site"
@@ -34,96 +33,122 @@ func main() {
 		return
 	}
 
-	app.Flags = []cli.Flag{
-		&cli.BoolFlag{
-			Name:        "verbose",
-			Destination: &debug,
-			Usage:       "Enable debug log level",
-		}, &cli.StringFlag{
-			Name:        "address",
-			Aliases:     []string{"a"},
-			Usage:       "Host for the HTTP server",
-			Value:       "0.0.0.0",
-			Destination: &address,
-		}, &cli.IntFlag{
-			Name:        "port",
-			Aliases:     []string{"p"},
-			Usage:       "port for the HTTP server",
-			Value:       8080,
-			Destination: &port,
-		}, &cli.StringFlag{
-			Name:        "directory",
-			Aliases:     []string{"d"},
-			Usage:       "Root directory for the HTTP server",
-			Value:       ".",
-			Destination: &root,
-		},
-	}
-
 	app.Commands = []*cli.Command{
 		{
 			Name:  "serve",
 			Usage: "Serve static web site",
-			Action: func(c *cli.Context) (err error) {
-				log.Print(fmt.Sprintf("serve %v on %v:%v", root, address, port))
-				http.Handle("/", http.FileServer(http.Dir(root)))
-				err = http.ListenAndServe(address, nil)
-				return
-			},
-			Subcommands: []*cli.Command{
-				{
-					Name:  "emailSupport",
-					Usage: "with email support",
-					Flags: []cli.Flag{
-						&cli.StringFlag{
-							Name:        "email",
-							Usage:       "Email address of sender",
-							Required:    true,
-							Destination: &emailAddress,
-						}, &cli.StringFlag{
-							Name:        "smtpLogin",
-							Usage:       "SMTP login",
-							Required:    true,
-							Destination: &smtpLogin,
-						}, &cli.StringFlag{
-							Name:        "smtpPassword",
-							Usage:       "SMTP password",
-							Required:    true,
-							Destination: &smtpPassword,
-						}, &cli.StringFlag{
-							Name:        "smtpHost",
-							Usage:       "SMTP Server Host",
-							Value:       "smtp.gmail.com",
-							Destination: &smtpHost,
-						}, &cli.IntFlag{
-							Name:        "smtpPort",
-							Usage:       "Sender Server port",
-							Value:       587,
-							Destination: &smtpPort,
-						},
-					},
-					Action: func(c *cli.Context) (err error) {
-						serverAddr := fmt.Sprintf("%v:%v", address, port)
-						log.Print(fmt.Sprintf("serve '%v' on 'http://%v' with email support '%v'",
-							root, serverAddr, emailAddress))
-						http.Handle("/_api/email", EmailSupport(
-							email.NewSender(emailAddress, smtpLogin, smtpPassword, smtpHost, smtpPort)))
-						http.Handle("/", http.FileServer(http.Dir(root)))
-						err = http.ListenAndServe(serverAddr, nil)
-						return
-					},
+			Flags: []cli.Flag{
+				&cli.BoolFlag{
+					Name:        "debug",
+					Aliases:     []string{"d"},
+					Destination: &debug,
+					Usage:       "Enable debug log level",
+				}, &cli.StringFlag{
+					Name:        "server",
+					Aliases:     []string{"a"},
+					Usage:       "Host for the HTTP server",
+					Value:       "",
+					Destination: &server,
+				}, &cli.IntFlag{
+					Name:        "port",
+					Aliases:     []string{"p"},
+					Usage:       "port for the HTTP server",
+					Value:       8080,
+					Destination: &port,
+				}, &cli.StringFlag{
+					Name:        "root",
+					Aliases:     []string{"r"},
+					Usage:       "Root directory for the HTTP server",
+					Value:       ".",
+					Destination: &root,
+				}, &cli.StringFlag{
+					Name:        "cors",
+					Usage:       "CORS, Access-Control-Allow-Origin pattern",
+					Destination: &cors,
 				},
+			},
+			Action: func(c *cli.Context) (err error) {
+
+				http.Handle("/", http.FileServer(http.Dir(root)))
+
+				serverAddr := fmt.Sprintf("%v:%v", server, port)
+
+				logrus.Infof("serve '%v' on 'http://%v'", root, serverAddr)
+				err = http.ListenAndServe(serverAddr, nil)
+
+				return
 			},
 		},
 		{
-			Name:  "markdown",
+			Name:  "serveWithEmailSupport",
+			Usage: "with email support",
+			Flags: []cli.Flag{
+				&cli.BoolFlag{
+					Name:        "debug",
+					Aliases:     []string{"d"},
+					Destination: &debug,
+					Usage:       "Enable debug log level",
+				},
+				&cli.StringFlag{
+					Name:        "config",
+					Aliases:     []string{"c"},
+					Usage:       "EmailBridge config file",
+					Value:       "config.xml",
+					Destination: &configFile,
+				},
+			},
+			Action: func(c *cli.Context) (err error) {
+				var config emailbridge.Config
+				if err = emailbridge.LoadConfig(configFile, &config); err == nil {
+					if _, err = emailbridge.NewEmailBridge(&config, http.DefaultServeMux); err == nil {
+
+						serverAddr := fmt.Sprintf("%v:%v", config.Server, config.Port)
+
+						linkHost := config.Server
+						if linkHost == "" {
+							linkHost = "localhost"
+						}
+
+						logrus.Infof("serve '%v' on 'http://%v:%v' with email support '%v'",
+							root, linkHost, port, config.Sender.Email)
+
+						fileServer := http.FileServer(http.Dir(config.Root))
+
+						http.Handle("/", fileServer)
+						err = http.ListenAndServe(serverAddr, nil)
+					}
+				}
+				return
+			},
+		},
+		{
+			Name:  "config",
+			Usage: "Generate default config file",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:        "target",
+					Aliases:     []string{"t"},
+					Usage:       "Config target file name to generate",
+					Value:       "config.yml",
+					Destination: &targetFile,
+				},
+			},
+			Action: func(c *cli.Context) (err error) {
+				config := emailbridge.BuildDefault()
+				config.Routes.Prefix = "_api/"
+				err = emailbridge.WriteConfig(targetFile, config)
+				return
+			},
+		},
+		{
+			Name:
+			"markdown",
 			Usage: "Generate markdown help file",
 			Flags: []cli.Flag{
 				&cli.StringFlag{
 					Name:        "target",
-					Aliases:     []string{"-t"},
+					Aliases:     []string{"t"},
 					Usage:       "Markdown target file name to generate",
-					Required:    true,
 					Value:       "site.md",
 					Destination: &targetFile,
 				},
