9dfd03640df338ae41b6d8041851a2b8f9c98d71
Eugen Eisler <eoeisler@siemens.com>
2021-06-22 17:06:51 +0200

    Extract Cli Class

--- site.go
+++ site.go
@@ -13,27 +13,44 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-func main() {
-	var server, root, configFile, targetFile, cors string
-	var debug bool
-	var port int
+func main()  {
+	app := NewCli()
 
-	app := cli.NewApp()
-	app.Name = "site"
-	app.Usage = "Web server for static web sites, like Hugo, with Email support"
-	app.Version = "1.0"
+	if err := app.Run(os.Args); err != nil {
+		logrus.WithFields(logrus.Fields{"err": err}).Warn("exit because of error.")
+	}
+}
+
+type Cli struct {
+	*cli.App
+	server, root, configFile, targetFile, cors string
+	debug                                      bool
+	port                                       int
+}
+
+func NewCli() (ret *Cli) {
+	ret = &Cli{}
+	ret.init()
+	return
+}
+
+func (o *Cli) init() {
+	o.App = cli.NewApp()
+	o.Name = "site"
+	o.Usage = "Web server for static web sites, like Hugo, with Email support"
+	o.Version = "1.0"
 
 	lg.LogrusTimeAsTimestampFormatter()
 
-	app.Before = func(c *cli.Context) (err error) {
-		if debug {
+	o.Before = func(c *cli.Context) (err error) {
+		if o.debug {
 			logrus.SetLevel(logrus.DebugLevel)
 		}
 		logrus.Debugf("execute %v", c.Command.Name)
 		return
 	}
 
-	app.Commands = []*cli.Command{
+	o.Commands = []*cli.Command{
 		{
 			Name:  "serve",
 			Usage: "Serve static web site",
@@ -41,43 +58,43 @@ func main() {
 				&cli.BoolFlag{
 					Name:        "debug",
 					Aliases:     []string{"d"},
-					Destination: &debug,
+					Destination: &o.debug,
 					Usage:       "Enable debug log level",
 				}, &cli.StringFlag{
 					Name:        "server",
 					Aliases:     []string{"a"},
 					Usage:       "Host for the HTTP server",
 					Value:       "",
-					Destination: &server,
+					Destination: &o.server,
 				}, &cli.IntFlag{
 					Name:        "port",
 					Aliases:     []string{"p"},
 					Usage:       "port for the HTTP server",
 					Value:       7070,
-					Destination: &port,
+					Destination: &o.port,
 				}, &cli.StringFlag{
 					Name:        "root",
 					Aliases:     []string{"r"},
 					Usage:       "Root directory for the HTTP server",
 					Value:       ".",
-					Destination: &root,
+					Destination: &o.root,
 				}, &cli.StringFlag{
 					Name:        "cors",
 					Usage:       "CORS, Access-Control-Allow-Origin pattern",
-					Destination: &cors,
+					Destination: &o.cors,
 				},
 			},
 			Action: func(c *cli.Context) (err error) {
 
-				http.Handle("/", http.FileServer(http.Dir(root)))
+				http.Handle("/", http.FileServer(http.Dir(o.root)))
 
-				serverAddr := fmt.Sprintf("%v:%v", server, port)
-				linkHost := server
+				serverAddr := fmt.Sprintf("%v:%v", o.server, o.port)
+				linkHost := o.server
 				if linkHost == "" || linkHost == "0.0.0.0" {
 					linkHost = "127.0.0.1"
 				}
 
-				logrus.Infof("serve '%v' on 'http://%v:%v'", root, linkHost, port)
+				logrus.Infof("serve '%v' on 'http://%v:%v'", o.root, linkHost, o.port)
 				err = http.ListenAndServe(serverAddr, nil)
 
 				return
@@ -90,7 +107,7 @@ func main() {
 				&cli.BoolFlag{
 					Name:        "debug",
 					Aliases:     []string{"d"},
-					Destination: &debug,
+					Destination: &o.debug,
 					Usage:       "Enable debug log level",
 				},
 				&cli.StringFlag{
@@ -98,12 +115,12 @@ func main() {
 					Aliases:     []string{"c"},
 					Usage:       "EmailBridge config file",
 					Value:       "config.yml",
-					Destination: &configFile,
+					Destination: &o.configFile,
 				},
 			},
 			Action: func(c *cli.Context) (err error) {
 				var config emailbridge.Config
-				if err = emailbridge.LoadConfig(configFile, &config); err == nil {
+				if err = emailbridge.ConfigLoad(o.configFile, &config); err == nil {
 					if _, err = emailbridge.NewEmailBridge(&config, http.DefaultServeMux); err == nil {
 
 						serverAddr := fmt.Sprintf("%v:%v", config.Server, config.Port)
@@ -114,9 +131,9 @@ func main() {
 						}
 
 						logrus.Infof("serve '%v' on 'http://%v:%v' with email support '%v'",
-							root, linkHost, port, config.Sender.Email)
+							o.root, linkHost, o.port, config.EngineConfig.Sender.Email)
 
-						fileServer := http.FileServer(http.Dir(config.Root))
+						fileServer := http.FileServer(http.Dir(config.StaticFolder))
 
 						http.Handle("/", fileServer)
 						err = http.ListenAndServe(serverAddr, nil)
@@ -134,13 +151,13 @@ func main() {
 					Aliases:     []string{"t"},
 					Usage:       "Config target file name to generate",
 					Value:       "config.yml",
-					Destination: &targetFile,
+					Destination: &o.targetFile,
 				},
 			},
 			Action: func(c *cli.Context) (err error) {
 				config := emailbridge.BuildDefault()
 				config.Routes.Prefix = "_api/"
-				err = config.WriteConfig(targetFile)
+				err = config.WriteConfig(o.targetFile)
 				return
 			},
 		},
@@ -154,21 +171,17 @@ func main() {
 					Aliases:     []string{"t"},
 					Usage:       "Markdown target file name to generate",
 					Value:       "site.md",
-					Destination: &targetFile,
+					Destination: &o.targetFile,
 				},
 			},
 			Action: func(c *cli.Context) (err error) {
-				if markdown, err := app.ToMarkdown(); err == nil {
-					err = ioutil.WriteFile(targetFile, []byte(markdown), 0)
+				if markdown, err := o.ToMarkdown(); err == nil {
+					err = ioutil.WriteFile(o.targetFile, []byte(markdown), 0)
 				} else {
 					logrus.Infof("%v", err)
 				}
 				return
 			},
 		},
-	}
-
-	if err := app.Run(os.Args); err != nil {
-		logrus.WithFields(logrus.Fields{"err": err}).Warn("exit because of error.")
 	}
 }
